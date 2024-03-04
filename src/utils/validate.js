58115fddee08642d4b4a7655b5ce8278f791ef6f
first project <rajkhoyani152@gmail.com>
2024-03-04 17:21:47 +0530

    done

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,10 +1,12 @@
-export function checkValidData(email, password) {
-     const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
-     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-     //const isNamevalid = /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(Name);
-     if (!isEmailValid) return "Email ID is not valid";
-     if (!isPasswordValid) return "Password is not valid";
+export const checkValidData = (email, password) => {
+  const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
+    email
+  );
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
+  if (!isEmailValid) return "Email ID is not valid";
+  if (!isPasswordValid) return "Password is not valid";
 
-     return null;
-}
+  return null;
+};
